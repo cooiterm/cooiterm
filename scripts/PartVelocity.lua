34aef96a7c853ff398c3876b12db0a22dfe124ab
CooITerm <koraedn2@gmail.com>
2025-01-09 14:37:11 -0800

    Update PartVelocity.lua

--- scripts/PartVelocity.lua
+++ scripts/PartVelocity.lua
@@ -147,7 +147,7 @@ local function tnoclip()
 end
 
 player.CharacterAdded:Connect(function(newCharacter)
-    local on = false
+    on = false
     character = newCharacter
     humanoidRootPart = character:WaitForChild("HumanoidRootPart")
     stop()
